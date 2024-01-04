77621cca4abdb327a6a4c1f00f6f00e24b3ea20f
karthi-prasad77 <karthiprasad44@gmail.com>
2024-01-04 15:31:02 +0530

    feat: Save functionality

--- FoodClassifier/Classifier/utils.py
+++ FoodClassifier/Classifier/utils.py
@@ -1,9 +1,19 @@
+"""
+Contains the model save and data download functionality
+"""
+
+
 import os
 import requests
 from pathlib import Path
 import zipfile
+import torch
 
 def data_download():
+    """
+        Download the data from the
+        online source
+    """
     # Setup path to data folder
     data_path = Path("data/")
     image_path = data_path / "pizza_steak_sushi"
@@ -30,3 +40,33 @@ def data_download():
     # Remove zip file
     os.remove(data_path / "pizza_steak_sushi.zip")
     print("Zip file removed after the unzip")
+
+
+def save_model(model:torch.nn.Module,
+               target_dir: str,
+               model_name: str):
+    """Saves a PyTorch model to a target directory.
+
+    Args:
+        model: A target PyTorch model to save.
+        target_dir: A directory for saving the model to.
+        model_name: A filename for the saved model. Should include
+        either ".pth" or ".pt" as the file extension.
+
+    Example usage:
+        save_model(model=model_0,
+                target_dir="models",
+                model_name="05_going_modular_tingvgg_model.pth")
+    """
+
+    # create an target directory
+    target_dir_path = Path(target_dir)
+    target_dir_path.mkdir(parents=True, exist_ok=True)
+
+    # create model save path
+    assert model_name.endswith(".pth") or model_name.endswith(".pt"), "model_name should end with '.pt' or '.pth'"
+    model_save_path = target_dir_path / model_name
+
+    # save the model state_dict()
+    print(f"[INFO] Saving model to: {model_save_path}")
+    torch.save(obj=model.state_dict(), f=model_save_path)
